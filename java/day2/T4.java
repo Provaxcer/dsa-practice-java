67ea9139db380f392fad7984cc11ecf0fdaa6b14
Provaxcer <basaksantanu659@gmail.com>
2026-04-06 19:02:49 +0530

    prime number done

--- java/day2/T4.java
+++ java/day2/T4.java
@@ -4,9 +4,21 @@ public class T4 {
     public static void main(String[] args) {
         Scanner scc = new Scanner(System.in);
         System.out.print("Enter Your Number: ");
-        boolean fin = scc.nextBoolean();
+        int num = scc.nextInt();
 
-        for (int i = 0; i<fin)
+        boolean isPrime = true;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime == true) {
+            System.out.println("prime");
+        } else {
+            System.out.println("Not Prime");
+        }
+
 
         scc.close();
     }
