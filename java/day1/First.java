d2e1dbfd9280ca4a6a09c4946030ef27bee0f39f
Provaxcer <basaksantanu659@gmail.com>
2026-06-24 18:10:21 +0530

    java

--- java/day1/First.java
+++
@@ -1,7 +0,0 @@
-public class First {
-    public static void main(String[] args) {
-        for(int i=1;i<=20;i++){
-            System.out.print(i + " ");
-        }
-    }
-}
--- java/day3/First.java
+++ java/day3/First.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class First {
-    public static void main (String[]args){
-        
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Type BTC Buy Price");
+        double buy = sc.nextDouble();
+
+        System.out.println("Type BTC Sell Price");
+        double sell = sc.nextDouble();
+
+        double pnl = sell - buy;
+        boolean isProfitable = pnl > 0;
+
+        System.out.println("PnL: " + pnl);
+        System.out.println("Profitable: " + isProfitable);
+
+        if (pnl > 1000 && isProfitable) {
+            System.out.println("Good trade!");
+        } else {
+            System.out.println("Keep improving.");
+        }
     }
 }
