9fdd3b01142a65a45fa001bafbe076bcdc64bf0d
Provaxcer <basaksantanu659@gmail.com>
2026-06-25 19:56:45 +0530

    java loops

---
+++ java/day4/Third.java
@@ -0,0 +1,9 @@
+public class Third {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println("Checking..." + i);
+            i++;
+        } while (i<5);
+    }
+}
