8c070b70c13cc493b572c1cb03669ca080a7764d
Provaxcer <basaksantanu659@gmail.com>
2026-06-30 18:45:08 +0530

    array java

---
+++ java/day5/Task2.java
@@ -0,0 +1,13 @@
+public class Task2 {
+    public static void main(String[] args) {
+        int [] arr = {10,20,30,40,50};
+
+        int sum = 0;
+
+        for (int i = 0; i<arr.length; i++) {
+            System.out.println(arr[i]);
+            sum = sum + arr[i];
+        }
+        System.out.println("Sum: " + sum);
+    }
+}
