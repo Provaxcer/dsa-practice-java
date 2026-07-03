929cfe0003a71c9e0f6ef20813228d43fd873ea2
Provaxcer <basaksantanu659@gmail.com>
2026-07-03 12:52:16 +0530

    2d array

--- java/day6/Task1.java
+++ java/day6/Task1.java
@@ -6,11 +6,14 @@ public class Task1{
             {22,5,45}
         };
 
+        int sum = 0;
         for (int i = 0; i< arr.length; i++){
             for (int j = 0; j < arr[i].length; j++){
             System.out.print(arr[i][j] + " ");
+            sum = sum + arr[i][j];
             }
             System.out.println();
         }
+        System.out.println("Sum: " + sum);
     }
 }
