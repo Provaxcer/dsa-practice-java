6d50ecf69520209c300f27faaf8b895358fabfff
Provaxcer <basaksantanu659@gmail.com>
2026-07-11 07:01:09 +0530

    jaava loop

---
+++ java/day1/Loop1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Loop1 {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Type your number: ");
+        int num = sc.nextInt();
+
+        for (int i= 1; i<=num;i++){
+            System.out.println(i);
+        }
+        sc.close();
+    }
+}
